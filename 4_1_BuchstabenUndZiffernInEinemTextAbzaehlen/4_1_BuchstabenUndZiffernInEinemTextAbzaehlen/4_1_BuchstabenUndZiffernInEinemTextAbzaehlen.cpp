4683b153f285a590f5c7beb02bd73f920ec976a4
tomtom1992 <hahn.thomas@fh-swf.de>
2017-11-07 17:30:34 +0100

    4.1 fertig

---
+++ 4_1_BuchstabenUndZiffernInEinemTextAbzaehlen/4_1_BuchstabenUndZiffernInEinemTextAbzaehlen/4_1_BuchstabenUndZiffernInEinemTextAbzaehlen.cpp
@@ -0,0 +1,70 @@
+/*=========================================================
+
+Filename: 4_1_BuchstabenUndZiffernInEinemTextAbzaehlen.cpp
+
+Author: Thomas Hahn
+
+Date: 07.11.2017
+
+Description: "Aufgabe: Buchstaben und Ziffern in einem Text abzählen"
+
+==========================================================*/
+
+
+#include "stdafx.h"
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int pow(int x, int y)
+{
+	if (y == 0)
+	{
+		return 1;
+	}
+	if (y == 1)
+	{
+		return x;
+	}
+	return x * pow(x, y - 1);
+}
+
+/*  description:    Hauptmethode des C++ Programms.
+parameters:			na
+return value:		Gibt 0 zurück, wenn das Programm ohne Fehler gelaufen ist.
+*/
+int main()
+{
+	int   maxstring = 256;                      // Puffergröße
+	char  text[256];                      // Eingabepuffer
+	char  c;                                    // Hilfsvariable
+	int   i, summekl, summegr, summezif, summeal;                  // Schleifenzähler,Buchstabezähler
+
+	cout << "***** Klein- und Großbuchstaben abzählen *****\n";
+	cout << "Geben Sie eine Textzeile ein: ";
+
+	i = 0;
+	while ((c = cin.get()) != '\n') {       // Einzelzeichen lesen
+		text[i] = c;                              //  und im Feld ablegen
+		i = i + 1;                            // Schleifenzähler erhöhen
+	}
+	text[i] = '\0';                             // Zeichenkette abschließen
+
+	cout << "Der eingegebene Text lautet '" << text << "'\n";
+
+	summekl = summegr = summezif = summeal = 0;
+	for (i = 0; text[i] != '\0'; i += 1) {
+		/*if (islower(text[i])) summekl = summekl + 1;
+		if (isupper(text[i])) summegr = summegr + 1;*/
+		if (isalpha(text[i])) summeal = summeal + 1;
+		if (isdigit(text[i])) summezif = summezif + 1;
+	}
+	/*cout << setw(6) << summekl << " Kleinbuchstaben\n";
+	cout << setw(6) << summegr << " Grossbuchstaben\n";*/
+	cout << setw(6) << summeal << " Buchstaben\n";
+	cout << setw(6) << summezif << " Ziffern\n";
+
+	return 0;
+}
+
